4e047b2ccdefc1557fc8cff0e28a1763837650e9
okaongok <okaongok@gmail.com>
2021-04-22 14:22:08 +0000

    s

--- public/index.php
+++ public/index.php
@@ -8,7 +8,15 @@ require_once(WWW_ROOT.'/../onlyou/App.php');
 use Onlyou\App;
 
 $config = [
-    
+    'db'=>[
+        'dsn'=>'mysql:host=localhost:3306;dbname=leiphone',
+        'user'=>'www',
+        'pass'=>'root',
+        'options'=>[
+            PDO::ATTR_PERSISTENT => true,
+            PDO::MYSQL_ATTR_INIT_COMMAND => "set names utf8"
+        ]
+    ]
 ];
 
 App::createApp($config,App::ENV_DEBUG);
